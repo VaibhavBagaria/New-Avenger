9777b4dc1aa3b8f97e93460489730afb09b3493f
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2021-11-12 09:16:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,154 +1,180 @@
-var canvas=new fabric.Canvas('myCanvas');
-blockImageObject="";
+var canvas = new fabric.Canvas ('myCanvas');
+blockImageObject = "";
 
-playerX=10;
-playerY=10;
-playerObject="";
-blockImageWidth=30;
-blockImageHeight=30;
+playerX = 10;
+playerY = 10;
+playerObject = "";
+blockImageWidth = 30;
+blockImageHeight = 30;
 
-function playerUpdate() {
-    fabric.Image.fromURL("player.png",function(Img){
-     playerObject=Img;
-     playerObject.scaleToWidth(50);
-     playerObject.scaleToHeight(150);
-     playerObject.set({
-         top:playerY,
-         left:playerX
+function playerUpdate () {
+    fabric.Image.fromURL ("player.png", function (Img) {
+     playerObject = Img;
+     playerObject.scaleToWidth (50);
+     playerObject.scaleToHeight (150);
+     playerObject.set ({
+         top: playerY,
+         left: playerX
      });
-     canvas.add(playerObject)
+     canvas.add (playerObject)
     });
 }
 
 
-function New_Image(getImage) {
-    fabric.Image.fromURL(getImage,function(Img){
-     blockImageObject=Img;
-     blockImageObject.scaleToWidth(blockImageWidth);
-     blockImageObject.scaleToHeight(blockImageHeight);
-     blockImageObject.set({
-         top:playerY,
-         left:playerX
+function New_Image (getImage) {
+    fabric.Image.fromURL (getImage, function (Img) {
+     blockImageObject = Img;
+     blockImageObject.scaleToWidth (blockImageWidth);
+     blockImageObject.scaleToHeight (blockImageHeight);
+     blockImageObject.set ({
+         top: playerY,
+         left: playerX
      });
-     canvas.add(blockImageObject)
+     canvas.add (blockImageObject)
     });
 }
 
-window.addEventListener("keydown", myKeyDown);
+window.addEventListener ("keydown", myKeyDown);
 
-function myKeyDown(e) {
+function myKeyDown (e) {
     keyPressed = e.keyCode;
-    console.log(keyPressed);
-    if (e.shiftKey == true && keyPressed == '80') {
+    console.log (keyPressed);
+    if (e.shiftKey == true && keyPressed == '90') {
         blockImageWidth = blockImageWidth + 10;
         blockImageHeight = blockImageHeight + 10;
-        document.getElementById("current_width").innerHTML = blockImageWidth;
-        document.getElementById("current_height").innerHTML = blockImageHeight;
+        document.getElementById ("current_width"). innerHTML = blockImageWidth;
+        document.getElementById ("current_height"). innerHTML = blockImageHeight;
     }
 
-    if (e.shiftKey == true && keyPressed == '77') {
-        if (blockImageWidth > 10) {
+    if (e.shiftKey == true && keyPressed == '89') {
+        if (blockImageWidth> 10) {
             blockImageWidth = blockImageWidth - 10;
             blockImageHeight = blockImageHeight - 10;
-            document.getElementById("current_width").innerHTML = blockImageWidth;
-            document.getElementById("current_height").innerHTML = blockImageHeight;
+            document.getElementById ("current_width"). innerHTML = blockImageWidth;
+            document.getElementById ("current_height"). innerHTML = blockImageHeight;
         }
     }
 
     if (keyPressed == '38') {
-        Up();
-        console.log("Up")
+        Up ();
+        console.log ("Up")
     }
     if (keyPressed == '40') {
-        Down();
-        console.log("down")
+        Down ();
+        console.log ("down")
     }
     if (keyPressed == '37') {
-        Left();
-        console.log("Left")
+        Left ();
+        console.log ("Left")
     }
     if (keyPressed == '39') {
-        Right();
-        console.log("Right")
+        Right ();
+        console.log ("Right")
     }
     if (keyPressed == '65') {
-        New_Image('hulk_head.png')
-        console.log("a")
+        New_Image ('hulk_face.png')
+        console.log ("a")
     }
     if (keyPressed == '66') {
-        New_Image('hulk_body.png')
-        console.log("b")
+        New_Image ('hulkd_body.png')
+        console.log ("b")
     }
     if (keyPressed == '67') {
-        New_Image('hulk_leg.png')
-        console.log("c")
+        New_Image ('hulk_legs.png')
+        console.log ("c")
     }
     if (keyPressed == '68') {
-        New_Image('hulk_right_hand.png')
-        console.log("d")
+        New_Image ('hulk_right_hand.png')
+        console.log ("d")
     }
     if (keyPressed == '69') {
-        New_Image('hulk_left_hand.png')
-        console.log("e")
+        New_Image ('hulk_left_hand.png')
+        console.log ("e")
     }
     if (keyPressed == '70') {
-        New_Image('ironman_head.png')
-        console.log("f")
+        New_Image ('ironman_face.png')
+        console.log ("f")
     }
     if (keyPressed == '71') {
-        New_Image('ironman_body.png')
-        console.log("g")
+        New_Image ('ironman_body.png')
+        console.log ("g")
     }
     if (keyPressed == '72') {
-        New_Image('ironman_leg.png')
-        console.log("h")
+        New_Image ('ironman_legs.png')
+        console.log ("h")
     }
     if (keyPressed == '73') {
-        New_Image('ironman_right_hand.png')
-        console.log("i")
+        New_Image ('ironman_right_hand.png')
+        console.log ("i")
     }
     if (keyPressed == '74') {
-        New_Image('ironman_left_hand.png')
-        console.log("j")
+        New_Image ('ironman_left_hand.png')
+        console.log ("y")
     }
     if (keyPressed == '75') {
-        New_Image('spiderman_head.png')
-        console.log("k")
+        New_Image ('spiderman_face.png')
+        console.log ("k")
     }
     if (keyPressed == '76') {
-        New_Image('spiderman_body.png')
-        console.log("l")
+        New_Image ('spiderman_body.png')
+        console.log ("l")
     }
     if (keyPressed == '77') {
-        New_Image('spiderman_leg.png')
-        console.log("m")
+        New_Image ('spiderman_legs.png')
+        console.log ("m")
     }
     if (keyPressed == '78') {
-        New_Image('spiderman_right_hand.png')
-        console.log("n")
+        New_Image ('spiderman_right_hand.png')
+        console.log ("n")
     }
     if (keyPressed == '79') {
-        New_Image('spiderman_left_hand.png')
-        console.log("o")
+        New_Image ('spiderman_left_hand.png')
+        console.log ("o")
     }
     if (keyPressed == '80') {
-        New_Image('thor_head.png')
-        console.log("p")
+        New_Image ('thor_face.png')
+        console.log ("p")
     }
     if (keyPressed == '81') {
-        New_Image('thor_body.png')
-        console.log("q")
+        New_Image ('thor_right_hand.png')
+        console.log ("q")
     }
     if (keyPressed == '82') {
-        New_Image('thor_leg.png')
-        console.log("r")
+        New_Image ('thor_left_hand.png')
+        console.log ("r")
     }
-    if (keyPressed == '83') {
-        New_Image('thor_right_hand.png')
-        console.log("s")
+}
+
+
+function Up () {
+    if (playerY> 0) {
+        playerY = playerY-10
+        console.log ('up')
+        canvas.remove (playerObject)
+        playerUpdate ()
     }
-    if (keyPressed == '84') {
-        New_Image('thor_left_hand.png')
-        console.log("t")
+}
+function Down () {
+    if (playerY <580) {
+        playerY = playerY + 10
+        console.log ('down')
+        canvas.remove (playerObject)
+        playerUpdate ()
+    }
+}
+function Right () {
+    if (playerX <780) {
+        playerX = playerX + 10
+        console.log ('right')
+        canvas.remove (playerObject)
+        playerUpdate ()
+    }
+}
+function Left () {
+    if (playerX> 0) {
+        playerX = playerX-10
+        console.log ('left')
+        canvas.remove (playerObject)
+        playerUpdate ()
     }
 }
